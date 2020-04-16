32985d428f5523288ca59a7d2d8bb2bdee6b622f
ah-aydin <ah-aydin@users.noreply.github.com>
2020-04-16 14:30:44 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("array-canvas")
+var startButton = document.getElementById('start')
 
 function init(){
     let dpi = window.devicePixelRatio
@@ -15,25 +16,36 @@ function init(){
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
 var alg = 'bubble'
 function change_alg(obj){
     alg = obj.value
+    startButton.innerHTML = alg.toUpperCase() + ' START'
     closeAllDropDown()
 }
 
-function sort()
-{
+async function sort(){
     switch (alg){
         case 'bubble':
-            bubbleSort(arr);
+            await bubbleSort(arr);
             break;
         case 'insertion':
-            insertionSort(arr);
+            await insertionSort(arr);
+            break;
+        case 'selection':
+            await selectionSort(arr);
+            break;
+        case 'merge':
+            await mergeSort(arr, 0, arr.length - 1)
+            break;
+        case 'quick':
+            await quickSort(arr, 0, arr.length - 1)
             break;
     }
-    
+    drawArr(arr, -1, -1, 'green')
+    await sleep(1000)
+    drawArr(arr)
 }
 
 async function bubbleSort(arr){
@@ -47,13 +59,10 @@ async function bubbleSort(arr){
                 arr[i] = arr[i + 1]
                 arr[i + 1] = temp
                 drawArr(arr, i + 1)
-                await sleep(50);
+                await sleep(100);
             }
         }
     } while (!sorted);
-    drawArr(arr, -1, 'green')
-    await sleep(100)
-    drawArr(arr)
 }
 
 async function insertionSort(arr){
@@ -64,16 +73,117 @@ async function insertionSort(arr){
             arr[j] = arr[j - 1]
             arr[j - 1] = temp
             drawArr(arr, j - 1)
-            await sleep(50)
+            await sleep(100)
             if (j - 1 == 0){
                 break
             }
             j--;
         }
     }
-    drawArr(arr, -1, 'green')
-    await sleep(1000)
+}
+
+async function selectionSort(arr){
+    for(var i = 0; i < arr.length; ++i){
+        var min = arr[i]
+        var minIndex = i
+        for (var j = i + 1; j < arr.length; ++j){
+            if (arr[j] < min){
+                min = arr[j]
+                minIndex = j
+            }
+        }
+        drawArr(arr, i, minIndex)
+        await sleep(50)
+        var temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+        drawArr(arr, minIndex, i)
+        await sleep(50)
+    }
+}
+
+async function merge(arr, left, mid, right){
+    var sizeLeft = mid - left + 1;
+    var sizeRight = right - mid;
+    let leftArr = []
+    let rightArr = []
+    for (var i = 0; i < sizeLeft; ++i){
+        leftArr.push(arr[left + i])
+    }
+    for (var i = 0; i < sizeRight; ++i){
+        rightArr.push(arr[mid + 1 + i])
+    }
+    var i = 0, j = 0;
+    while (i < sizeLeft && j < sizeRight){
+        if (leftArr[i] < rightArr[j]){
+            arr[left + i + j] = leftArr[i]
+            ++i
+        }
+        else{
+            arr[left + i + j] = rightArr[j]
+            ++j
+        }
+    }
+    while (i < sizeLeft){
+        arr[left + i + j] = leftArr[i]
+        ++i
+    }
+    while (j < sizeRight){
+        arr[left + i + j] = rightArr[j]
+        ++j
+    }
     drawArr(arr)
+    await sleep(100)
+}
+
+async function mergeSort(arr, left, right){
+    if (left >= right) return
+    var mid = parseInt((left + right) / 2)
+    await mergeSort(arr, left, mid)
+    await mergeSort(arr, mid + 1, right)
+    await merge(arr, left, mid, right)
+}
+
+async function quickSort(arr, left, right){
+    if (left >= right) return;
+
+    var pivot = parseInt((left + right) / 2)
+    var n_move = 0
+    for (var i = pivot - 1; i >= left; --i){
+        if (arr[pivot] < arr[i]){
+            ++n_move;
+            drawArr(arr, pivot - n_move, i)
+            await sleep(50)
+            var temp = arr[pivot - n_move]
+            arr[pivot - n_move] = arr[i]
+            arr[i] = temp
+            drawArr(arr, pivot - n_move, i)
+            await sleep(50)
+        }
+    }
+    pivot -= n_move
+    n_move = 0
+    for (var i = pivot + 1; i <= right; ++i){
+        if (arr[pivot] > arr[i]){
+            ++n_move
+            drawArr(arr, pivot + n_move, i)
+            await sleep(50)
+            var temp = arr[pivot + n_move]
+            arr[pivot + n_move] = arr[i]
+            arr[i] = temp
+            drawArr(arr, pivot + n_move, i)
+            await sleep(50)
+        }
+    }
+    var temp = arr[pivot]
+    arr[pivot] = arr[pivot + n_move]
+    arr[pivot + n_move] = temp
+    drawArr(arr, pivot)
+    await sleep(50)
+
+    await quickSort(arr, left, pivot + n_move - 1)
+    await quickSort(arr, pivot + n_move + 1, right)
+
 }
 
 var arr = []
@@ -91,7 +201,7 @@ function constructArray(){
     drawArr(arr)
 }
 
-function drawArr(arr, movedpiece=-1, color=-1){
+function drawArr(arr, movedpiece=-1, movedpiece2=-1, color=-1){
     var ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -107,6 +217,9 @@ function drawArr(arr, movedpiece=-1, color=-1){
         ctx.fillStyle = "#282828";
         if (movedpiece == i){
             ctx.fillStyle = "#FF0000"
+        }
+        if (movedpiece2 == i){
+            ctx.fillStyle = "#0000FF"
         }
         if (color != -1){
             ctx.fillStyle = color
